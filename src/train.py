39ddf5db1205f5ecd866b05868d906ee7402b3d6
Misha Zhukov <3v1l91l@gmail.com>
2018-01-17 09:13:57 +0200

    Read test data

--- src/train.py
+++ src/train.py
@@ -12,37 +12,53 @@ from skimage import transform
 from keras.callbacks import ModelCheckpoint
 import pandas as pd
 
-IMG_WIDTH = 256
+IMG_WIDTH = IMG_HEIGHT = 128
+IMG_CHANNELS = 3
 
 def img_resize(img):
     return transform.resize(img, (IMG_WIDTH, IMG_WIDTH), preserve_range=True)
 
 def load_data(root_dir):
+
     img_ids = os.listdir(root_dir)[:100]
     img_ids = [x for x in img_ids if not x.startswith('.')]
-    images = masks = []
-    for img_id in img_ids:
-        image = skimage.io.imread(os.path.join(root_dir, img_id, 'images', img_id +".png"))
-        mask = skimage.io.imread_collection(os.path.join(root_dir, img_id, 'masks', '*.png')).concatenate()
-        # if image.shape[0] != IMG_WIDTH:
-        #     image = img_resize(image)
-        #     mask = list(map(img_resize, mask))
-        overlay_mask = np.zeros((image.shape[0], image.shape[1]), np.bool)
-        for index in range(len(mask)):
-            overlay_mask[mask[index]>0] = True
-        images.append(image)
-        masks.append(overlay_mask)
-    return images, masks
+    X = np.zeros((len(img_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+    Y = np.zeros((len(img_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+
+    for i, img_id in enumerate(img_ids):
+        image = skimage.io.imread(os.path.join(root_dir, img_id, 'images', img_id +".png"))[:,:,:IMG_CHANNELS]
+        masks = skimage.io.imread_collection(os.path.join(root_dir, img_id, 'masks', '*.png')).concatenate()
+        image = resize(image, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        X[i] = image
+
+        overlay_mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+        for mask in masks:
+            mask = np.expand_dims(resize(mask, (IMG_HEIGHT, IMG_WIDTH), mode='constant',
+                                          preserve_range=True), axis=-1)
+            overlay_mask = np.maximum(mask, overlay_mask)
+        Y[i] = overlay_mask
+    return X, Y
 
 def test_data(root_dir):
     img_ids = os.listdir(root_dir)
     img_ids = [x for x in img_ids if not x.startswith('.')]
+
+    X = np.zeros((len(img_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+    sizes = []
+    for i, img_id in enumerate(img_ids):
+        image = skimage.io.imread(os.path.join(root_dir, img_id, 'images', img_id +".png"))[:,:,:IMG_CHANNELS]
+        sizes.append([image.shape[0], image.shape[1]])
+        image = resize(image, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        X[i] = image
+    return X, sizes
+
     images = []
     for img_id in img_ids:
         image = skimage.io.imread(os.path.join(root_dir, img_id, 'images', img_id + ".png"))
         images.append(image)
     train_img_df = pd.DataFrame({'images': images})
     print(train_img_df['images'].map(lambda x: x.shape).value_counts())
+
 
 
 def train():
